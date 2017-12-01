06d9ed72da8253cf34832f68cf51d75d16897976
Tengis B <tengis617@users.noreply.github.com>
2017-12-01 14:42:41 +0700

    created actions

---
+++ tw-slack-bot/actions.js
@@ -0,0 +1,11 @@
+'use strict'
+
+const COMPLETED = 'completed'
+const ASSIGN_MEMBERS = 'assign_members'
+const DUE_DATE = 'due_date'
+
+module.exports = {
+    COMPLETED,
+    ASSIGN_MEMBERS,
+    DUE_DATE
+}
